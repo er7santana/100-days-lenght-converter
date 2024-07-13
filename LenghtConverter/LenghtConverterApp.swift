6e8fb01c3a90471e8dae50b82e624773d9a422e5
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-13 18:53:15 -0300

    Initial Commit

---
+++ LenghtConverter/LenghtConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenghtConverterApp.swift
+//  LenghtConverter
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LenghtConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
